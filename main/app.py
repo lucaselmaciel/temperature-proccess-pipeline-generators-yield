367bb501227b0d34117bbc4e24540bc5b7f93377
Oliver <lucasmaciel63@gmail.com>
2025-08-15 20:15:04 -0300

    remove line strip

--- main/app.py
+++ main/app.py
@@ -3,7 +3,7 @@ from main.pipeline import pipeline
 
 if __name__ == "__main__":
     with open("main/paths.txt") as file:
-        paths = [line.strip() for line in file if line.strip()]
+        paths = [line.strip() for line in file]
 
     for row in pipeline(paths):
         print(row)
